3011714abfb3dd93621ca044c1ff44a2f13c06b0
Ishan Goyal <63078216+Ishan27g@users.noreply.github.com>
2021-11-27 02:23:57 +1100

    fix event

--- event.go
+++ event.go
@@ -1,92 +1,138 @@
 package vClock
 
-import "github.com/emirpasic/gods/lists/arraylist"
+import (
+	"github.com/emirpasic/gods/lists/arraylist"
+	avl "github.com/emirpasic/gods/trees/avltree"
+)
 
+// Events : provides interface for a process with lease/leader-role
+// receives `new-events` from followers with eventId and followers vector clock
 type Events interface {
-	NewEvent(eventIdOrHash string, v2 VClock)
-	// GetEventsOrder returns the events ordered according to vector clock
-	// for the events
+	// MergeEventClock takes an eventId & clock and merges with existing clock
+	MergeEventClock(eventIdOrHash string, v2 EventClock)
+	// GetEventsOrder returns the eventIds ordered according to vector clock for the events
 	GetEventsOrder() (eventIdsOrHashes []string)
 }
 
-// Event Every event is saved with the vector clock of when that event was received
-type Event struct {
-	events map[string]*VClock
-	eventList *arraylist.List
+
+// EventClock is vector-clock of peer-address and its individual clock
+type EventClock map[string]int
+
+// value for eventClocks tree
+type event struct {
+	eventId    string
+	eventClock EventClock
 }
-func (e *Event) GetEventsOrder() (eventIdsOrHashes []string) {
-	e.sort()
-	var happenedBefore []string
-	e.eventList.Each(func(_ int, value interface{}) {
-		e := value.(event)
-		happenedBefore = append(happenedBefore, e.eventIdOrHash)
-	})
-	return happenedBefore
-}
-func (e *Event) sort() {
-	e.eventList.Sort(func(a, b interface{}) int {
-		e1 := a.(event)
-		e2 := a.(event)
-		c1 := compareClock(e1.clock, e2.clock)
-		c2 := compareClock(e2.clock, e1.clock)
-		if c1 && c2 { // both are same
-			return 0
-		} else if c1 && !c2 { // e1 happened before
-			return -1
-		} else {
-			return 1
-		}
-	})
+// all events
+type events struct {
+	eventClocks *avl.Tree // key = eventIdOrHash, value = EventClock
 }
 
-// NewEvent merges the current vector clock for this event with the received vector clock
-func (e *Event) NewEvent(eventIdOrHash string, v2 VClock) {
-	if e.events[eventIdOrHash] == nil{
-		e.events[eventIdOrHash] = &v2
-		e.eventList.Add(event{
-			eventIdOrHash: eventIdOrHash,
-			clock:         v2,
-		})
+// merge entries in v1 with those found in v2
+func merge(v1, v2 EventClock) EventClock {
+	v := make(EventClock)
+	for s, i := range v1 {
+		if v2[s] == 0 && i != 0 { // in v1 and not in v2
+			v[s] = i
+		}
+		if v2[s] < i  { // in v1 and not in v2
+			v[s] = i
+		}else {
+			v[s] = v2[s]
+		}
+	}
+	return v
+}
+func (v1 *EventClock)mergeWith(v2 EventClock)*EventClock {
+	 v := merge(*v1, v2)
+	 v = merge(v2, v)
+	 return &v
+}
+
+func newEvent(eventIdOrHash string, v2 EventClock) event {
+	return event{
+		eventId:    eventIdOrHash,
+		eventClock: v2,
+	}
+}
+
+func (e *events) MergeEventClock(eventIdOrHash string, v2 EventClock) {
+	// check if present with another vectorClock
+	v1, found := e.eventClocks.Get(eventIdOrHash)
+	if !found {
+		// new entry
+		e.eventClocks.Put(eventIdOrHash, newEvent(eventIdOrHash, v2))
 		return
 	}
-	eventClock := *e.events[eventIdOrHash]
-	if eventClock == nil {
-
-	}
-	e.mergeClock(eventIdOrHash, v2)
-}
-func (e *Event) mergeClock(eventIdOrHash string, v2 VClock) {
-	eventClock := *e.events[eventIdOrHash]
-	for addr, clock := range v2 {
-		if eventClock[addr] < clock {
-			eventClock[addr] = clock
-		}
-	}
-	for addr, clock := range eventClock {
-		if v2[addr] > clock {
-			eventClock[addr] = clock
-		}
-	}
-	e.events[eventIdOrHash] = &eventClock
-
-	newList := arraylist.New()
-	for it := e.eventList.Iterator(); it.Next(); {
-		e:= it.Value().(event)
-		if e.eventIdOrHash == eventIdOrHash{
-			newList.Add(event{
-					eventIdOrHash: eventIdOrHash,
-					clock:         eventClock,
-				})
-		}else {
-			newList.Add(e)
-		}
-	}
+	// get existing EventClock
+	v := v1.(event)
+	// merge v1 and v2
+	v.eventClock = *v.eventClock.mergeWith(v2)
+	// update eventClocks
+	e.eventClocks.Put(eventIdOrHash, v)
 }
 
-func NewEventVectorClock() Events{
-	e := Event{
-		events: map[string]*VClock{},
-		eventList: arraylist.New(),
+func (e *events) GetEventsOrder() []string {
+	k := e.eventClocks.Values()
+	a := arraylist.New()
+	for i := 0; i < len(k); i++ {
+		ec := k[i].(event)
+		a.Add(ec)
+	}
+	a.Sort(eventComparator)
+	var eventIdsOrHashes []string
+	a.Each(func(_ int, value interface{}) {
+		ec := value.(event)
+		eventIdsOrHashes = append(eventIdsOrHashes, ec.eventId)
+	})
+
+	return eventIdsOrHashes
+}
+
+var eventComparator = func(a, b interface{}) int {
+	v1 := a.(event)
+	v2 := b.(event)
+	c1 := compareClock(v1.eventClock, v2.eventClock)
+	c2 := compareClock(v2.eventClock, v1.eventClock)
+	if c1 && c2 { // both are same
+		return 0
+	} else if c1 && !c2 { // e1 happened before
+		return -1
+	} else {
+		return 1
+	}
+}
+func NewEventVector(self string, peers []string) Events {
+	e := events{
+		eventClocks: avl.NewWithStringComparator(),
 	}
 	return &e
+}
+
+
+// compareClock returns true if v1 is before or concurrent to v2
+func compareClock(v1, v2 EventClock) bool {
+	if len(v1) != len(v2) {
+		return false
+	}
+	v1IsBefore := true
+	for addr, v1Clock := range v1 {
+		v2Clock := v2[addr]
+		if v2Clock == 0 && v1Clock != 0{
+			continue
+		}
+		if v2Clock < v1Clock {
+			v1IsBefore = false
+		}
+	}
+	for addr, v2Clock := range v2 {
+		v1Clock := v1[addr]
+		if v1Clock == 0 && v2Clock != 0{
+			continue
+		}
+		if v2Clock < v1Clock {
+			v1IsBefore = false
+		}
+	}
+	return v1IsBefore
 }
--- vClock.go
+++ vClock.go
@@ -10,39 +10,29 @@ import (
 // VectorClock provides interface to implement vector clock
 type VectorClock interface {
 	// Get returns the current vector clock
-	Get() VClock
+	Get() EventClock
 
 	// SendEvent returns the current vector clock after first updating self clock and then
 	// updating the individual clocks for corresponding addresses
-	SendEvent(eventIdOrHash string, addresses []string) VClock
+	SendEvent(eventIdOrHash string, addresses []string) EventClock
 
 	// ReceiveEvent updates the current vector clock using element wise maximum with the passed vector clock
-	ReceiveEvent(eventIdOrHash string, v VClock)
-
-	// GetEventsOrder returns the events ordered according to vector clock
-	// for the events
-	GetEventsOrder() (eventIdsOrHashes []string)
+	ReceiveEvent(eventIdOrHash string, v EventClock)
 
 	Clear()
 	print()
 }
 
-// VClock is a map of peer-address and its individual clock
-type VClock map[string]int
+// EventClock is a map of peer-address and its individual clock
 
-type event struct {
-	eventIdOrHash string
-	clock VClock
-}
 type vClock struct {
 	lock sync.Mutex
-	self string
-	vectorClock VClock
+	self        string
+	vectorClock EventClock
 	addressList *arraylist.List
-	events *arraylist.List
 }
 
-func (v *vClock) AddEvent(eventIdOrHash string, v1 VClock) {
+func (v *vClock) AddEvent(eventIdOrHash string, v1 EventClock) {
 	v.lock.Lock()
 	defer v.lock.Unlock()
 	// merge with received clock
@@ -54,10 +44,7 @@ func (v *vClock) AddEvent(eventIdOrHash string, v1 VClock) {
 			v.updateClock(address, newClock)
 		}
 	}
-	v.events.Add(event{
-		eventIdOrHash: eventIdOrHash,
-		clock:         v1,
-	})
+
 }
 
 func (v *vClock) Clear() {
@@ -65,70 +52,33 @@ func (v *vClock) Clear() {
 	v.vectorClock = nil
 	v.addressList.Clear()
 	v.addressList = nil
-	v.events = nil
 	v.lock.Unlock()
 }
 
 func (v *vClock) print() {
-	fmt.Println(v.events.String())
-	// for a, c := range v.vectorClock {
-	//	fmt.Println(a, c)
-	// }
+	for a, c := range v.vectorClock {
+		fmt.Println(a, c)
+	}
 }
 
 // Get returns the current vector clock
-func (v *vClock) Get() VClock {
+func (v *vClock) Get() EventClock {
 	return v.vectorClock
 }
 
-
-// GetEventsOrder returns the events ordered according to vector clock
-// for the events
-func (v *vClock) GetEventsOrder() (eventIdsOrHashes []string) {
-	v.sortEvents()
-	var happenedBefore []string
-	v.events.Each(func(_ int, value interface{}) {
-		e := value.(event)
-		happenedBefore = append(happenedBefore, e.eventIdOrHash)
-	})
-	return happenedBefore
-}
-
-// sortEvents sorts the events according to happened before relation
-func (v *vClock)sortEvents(){
-	v.events.Sort(func(a, b interface{}) int {
-		e1 := a.(event)
-		e2 := a.(event)
-		c1 := compareClock(e1.clock, e2.clock)
-		c2 := compareClock(e2.clock, e1.clock)
-		if c1 && c2 { // both are same
-			return 0
-		} else if c1 && !c2 { // e1 happened before
-			return -1
-		} else {
-			return 1
-		}
-	})
-}
-
 // SendEvent returns the current vector clock after updating the individual clocks for these entries
-func (v *vClock) SendEvent(eventIdOrHash string, address []string) VClock{
+func (v *vClock) SendEvent(eventIdOrHash string, address []string) EventClock {
 	v.lock.Lock()
 	defer v.lock.Unlock()
 	// update the individual clock entry for self
 	v.event(v.self)
-
 	for _, a := range address {
 		v.event(a)
 	}
-	v.events.Add(event{
-		eventIdOrHash: eventIdOrHash,
-		clock:         v.Get(),
-	})
 	return v.Get()
 }
 // ReceiveEvent updates the current vector clock using element wise maximum with the passed vector clock
-func (v *vClock) ReceiveEvent(eventIdOrHash string, v1 VClock) {
+func (v *vClock) ReceiveEvent(eventIdOrHash string, v1 EventClock) {
 	v.lock.Lock()
 	defer v.lock.Unlock()
 	// update local clock
@@ -142,10 +92,6 @@ func (v *vClock) ReceiveEvent(eventIdOrHash string, v1 VClock) {
 			v.updateClock(address, newClock)
 		}
 	}
-	v.events.Add(event{
-		eventIdOrHash: eventIdOrHash,
-		clock:         v.Get(),
-	})
 }
 // event updates the individual clock entry for this entry
 func (v *vClock) event(address string) {
@@ -165,7 +111,6 @@ func Init(self string, peers []string) VectorClock {
 		vectorClock: make(map[string]int),
 		self: self,
 		addressList: arraylist.New(),
-		events: arraylist.New(),
 	}
 
 	for _, peer := range peers {
@@ -179,31 +124,4 @@ func Init(self string, peers []string) VectorClock {
 func (v *vClock)initClock(peer string) {
 	v.vectorClock[peer] = 0
 	v.addressList.Add(peer)
-}
-
-// compareClock returns true if v1 is before or concurrent to v2
-func compareClock(v1 VClock, v2 VClock) bool {
-	if len(v1) != len(v2) {
-		return false
-	}
-	v1IsBefore := true
-	for addr, v1Clock := range v1 {
-		v2Clock := v2[addr]
-		if v2Clock == 0 && v1Clock != 0{
-			continue
-		}
-		if v2Clock < v1Clock {
-			v1IsBefore = false
-		}
-	}
-	for addr, v2Clock := range v2 {
-		v1Clock := v1[addr]
-		if v1Clock == 0 && v2Clock != 0{
-			continue
-		}
-		if v2Clock < v1Clock {
-			v1IsBefore = false
-		}
-	}
-	return v1IsBefore
 }
